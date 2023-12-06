3c4a2f1f420570160a0fd5b981d12fe2612a47d3
Alvaro <Alvaro.Parra2@alu.uclm.es>
2023-12-06 11:18:12 +0100

    Corrección en el PostMapping

--- src/main/java/es/uclm/GestiBiblioteca/business/controller/GestorPenalizaciones.java
+++ src/main/java/es/uclm/GestiBiblioteca/business/controller/GestorPenalizaciones.java
@@ -1,31 +1,45 @@
 package es.uclm.GestiBiblioteca.business.controller;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 import es.uclm.GestiBiblioteca.business.entities.Usuario;
 import es.uclm.GestiBiblioteca.persistence.UsuarioDAO;
-
+import org.springframework.ui.ModelMap;
 public class GestorPenalizaciones {
 
 	UsuarioDAO usuarioDAO;
+	private static final Logger log = (Logger) LoggerFactory.getLogger(GestorPenalizaciones.class);
 
 	/**
 	 * 
 	 * @param u
 	 */
 	@GetMapping("/aplicarPenalizacion")
-	public String mostrarFormularioPenalizaciones(Model model) {
-	    List<Usuario> usuarios = usuarioDAO.findAll();
-	    
-	    log.info("Usuarios: " + usuarios);
+	public String mostrarFormularioPenalizaciones(ModelMap model) {
+		List<Usuario> usuarios = usuarioDAO.findAll();
+		
+		log.info("Usuarios: " + usuarios);
 
-	    model.addAttribute("usuarios", usuarios);
+		model.addAttribute("usuarios", usuarios);
 
-	    return "/aplicarPenalizacion"; 
+		return "/aplicarPenalizacion"; 
 	}
 
 	@PostMapping("/aplicarPenalizacion")
 	public String aplicarPenalizacion(@ModelAttribute("usuario") Usuario u, 
 	                                   @RequestParam("fechaPenalizacion") @DateTimeFormat(pattern = "yyyy-MM-dd") Date fechaPenalizacion,
-	                                   Model model) { 
+	                                   ModelMap model) { 
 	    
 	    try {
 	        // Cargar al usuario desde la base de datos utilizando el ID del usuario
@@ -34,23 +48,23 @@ public class GestorPenalizaciones {
 	        if (optionalUser.isPresent()) {
 	            Usuario existingUser = optionalUser.get();
 
-	            // Aplicar la penalización (actualizar fecha de penalización)
+	            // Aplicar la penalizaciï¿½n (actualizar fecha de penalizaciï¿½n)
 	            existingUser.setFechaFinPenalizacion(fechaPenalizacion);
 
 	            // Guardar el usuario actualizado de nuevo en la base de datos
 	            usuarioDAO.save(existingUser);
 
-	            // Devolver un mensaje de éxito
-	            model.addAttribute("mensaje", "Penalización realizada exitosamente.");
-	            log.info("Penalización aplicada a Usuario con ID: " + existingUser.getId());
+	            // Devolver un mensaje de ï¿½xito
+	            model.addAttribute("mensaje", "Penalizaciï¿½n realizada exitosamente.");
+	            log.info("Penalizaciï¿½n aplicada a Usuario con ID: " + existingUser.getId());
 	        } else {
 	            // Manejar el caso en el que no se encuentra al usuario con el ID proporcionado
-	            model.addAttribute("mensaje", "No se ha podido aplicar la penalización, ID no encontrado.");
+	            model.addAttribute("mensaje", "No se ha podido aplicar la penalizaciï¿½n, ID no encontrado.");
 	        }
 	    } catch (Exception e) {
 	        //  errores de base de datos
-	        log.error("Error aplicando penalización", e);
-	        model.addAttribute("mensaje", "Error al aplicar la penalización.");
+	        log.error("Error aplicando penalizaciï¿½n", e);
+	        model.addAttribute("mensaje", "Error al aplicar la penalizaciï¿½n.");
 	    }
 
 	    return "/resultPenalizacion"; 
